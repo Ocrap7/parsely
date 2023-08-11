74e78ea468d8908f7e02d35140cd02592426c5e3
Oliver Clarke <oliverhenryc@gmail.com>
2023-08-11 07:49:11 -0500

    Delete random stuff

--- out.ts
+++
@@ -1,5 +0,0 @@
-export type Input = {
-value: number
-}
-import Context from './context.ts'
-export default function render<C extends Context>(ctx: C, { value, }: Input) {return `<html><head><title>This title</title></head><body><a href="/route">${5}Goto route</a></body></html>`}
